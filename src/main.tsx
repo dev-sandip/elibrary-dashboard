258ea651fe3b3575c78c439329d484f0a420e0d5
Sandip Sapkota <079bct076@ioepc.edu.np>
2024-05-01 21:20:04 +0545

    ✨feature: Added Login Page

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+
 import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.tsx";
@@ -8,6 +8,5 @@ import router from "./router.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
-    <App />
   </React.StrictMode>
 );
